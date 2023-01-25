8b4748011c8db59d2c27b74c9637c691d4323fc3
babar <babersdn@gmail.com>
2023-01-25 22:03:00 +0500

    edited Home Comp

--- vite-project/src/Components/Home.jsx
+++ vite-project/src/Components/Home.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
 function Home() {
-  return (
-    <div>Home</div>
+  return (<div className='prnt-comp container-fluid w-75 rounded-2 my-3 p-3 text-center border '>
+  <h3>Home Component</h3>
+  
+  </div>
   )
 }
 
