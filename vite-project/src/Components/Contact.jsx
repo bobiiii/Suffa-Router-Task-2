bf336583a418981875c77a860bffa90ca415fffe
babar <babersdn@gmail.com>
2023-01-25 21:57:48 +0500

    Completed Suffah assignment

---
+++ vite-project/src/Components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div className='prnt-comp container-fluid w-75 rounded-2 my-3 p-3 text-center border '>
+    <h3>Contact Component</h3>
+    
+    </div>
+  )
+}
+
+export default Contact
